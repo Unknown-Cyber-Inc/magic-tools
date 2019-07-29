efafee34af085ccbd716556d62f39f828e116449
Arun Lakhotia <arun@cythereal.com>
2019-07-29 18:05:52 -0500

    Add --threshold CLI option
    Why? Make it convenient to select the similarity threshold to use.

--- magic_diff.py
+++ magic_diff.py
@@ -81,12 +81,15 @@ default_listfile = None
 default_proxy_store = os.path.join(os.path.dirname(__file__), "data", "magic_cache", "genomics")
 default_display_hash = "sha1"
 default_other_columns = ""
+default_threshold = 0.05
 
 def process_args():
     from optparse import OptionParser
     parser = OptionParser()
     parser.add_option("--lf", "--list-file", dest="listfile", default=default_listfile,
                       help="File with list of sha1s to diff. Default is: %s" % default_listfile)
+    parser.add_option("--threshold", dest="threshold", default=default_threshold,
+                      help="Threshold (of minimum) similarity to display in result. Default is: %s" % default_threshold)
     parser.add_option("--cache", dest="proxy_store", default=default_proxy_store,
                       help="Directory to cache data from querying MAGIC. Default is: %s" % default_proxy_store)
     parser.add_option("--display-hash", dest="display_hash", default=default_display_hash,
@@ -130,7 +133,7 @@ def pick (args, i, other_labels=[]):
 
 if __name__ == "__main__":
     options, args = process_args()
-    threshold = 0.05
+    threshold = float(options.threshold)
     other_columns = options.other_columns
     output_sep = ","
     bindiff = BinDiff(options.proxy_store)
